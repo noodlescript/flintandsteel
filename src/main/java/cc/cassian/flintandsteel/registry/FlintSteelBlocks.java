c694d2233c42a10484d3abc82a885fbe83c7b5e2
noodlescript <tubbseric@gmail.com>
2026-08-30 19:24:38 -0500

    actually builds now

--- src/main/java/cc/cassian/flintandsteel/registry/FlintSteelBlocks.java
+++ src/main/java/cc/cassian/flintandsteel/registry/FlintSteelBlocks.java
@@ -1,6 +1,5 @@
 package cc.cassian.flintandsteel.registry;
 
-import com.teamabnormals.atmospheric.core.other.AtmosphericProperties;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.LeavesBlock;
 
